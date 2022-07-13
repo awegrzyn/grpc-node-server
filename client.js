af7ac124b9a9f972d9d3eeb6238b70f7b1d8aafe
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2022-07-13 17:49:55 +0200

    indent

--- client.js
+++ client.js
@@ -6,9 +6,9 @@ const proto = grpc.loadPackageDefinition(packageDefinition).o2bookkeeping;
 const client = new proto.KeyValueService('127.0.0.1:50051', grpc.credentials.createInsecure());
 
 client.UpdateValues({kv: [{key: 'key1', value: 'value1'}, {key: 'key2', value: 'value2'}]}, (err, response) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(JSON.stringify(response));
+ if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.stringify(response));
 	}
 });
